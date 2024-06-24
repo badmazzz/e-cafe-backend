868ea0670b0dd5543bb2e21f40e35a2f275f6b71
Meshv Patel <84256381+badmazzz@users.noreply.github.com>
2024-06-24 14:41:43 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,8 +14,6 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-
-
 // Import router
 
 import userRouter from "./routes/user.routes.js";
