d029d7d262c3a47f18665b992e7a95566b355e1c
Meshv Patel <84256381+badmazzz@users.noreply.github.com>
2024-06-24 13:12:06 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/badmazzz`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log("MONGODB Connected")
         
     } catch (error) {
