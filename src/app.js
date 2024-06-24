bc4f18c1639f2d7248ff653e35568bf42ee9b05e
Meshv Patel <84256381+badmazzz@users.noreply.github.com>
2024-06-24 14:34:37 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://e-cafe.onrender.com",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
