fd89fdb5697348444e1d35a26061517f9f68fd2c
Meshv Patel <84256381+badmazzz@users.noreply.github.com>
2024-06-24 12:46:09 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,15 +4,11 @@ import cors from "cors";
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://zippy-pudding-4c24a6.netlify.app",
   credentials: true,
 };
 
 app.use(cors(corsOptions));
-app.use(cors({
-  origin: 'https://zippy-pudding-4c24a6.netlify.app',
-  credentials: true,
-}));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
