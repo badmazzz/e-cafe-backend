1e740f3113f8f280207c0e57a16e77144324354f
Meshv Patel <84256381+badmazzz@users.noreply.github.com>
2024-06-24 13:53:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,15 +4,17 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({
+const corsOptions = {
   origin: "https://capable-cupcake-ddc33c.netlify.app",
   credentials: true,
-}));
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser());
 
 
 // Import router
